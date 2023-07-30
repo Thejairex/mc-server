c2d8aec9a31538ca22d528dd79d4fdcdc20c5272
Thejairex <yairjesus777@gmail.com>
2023-07-30 20:15:07 -0300

    test library

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-import mcpi.minecraft
+from mcpi.minecraft import Minecraft
+
+mc = Minecraft.create(address="186.125.41.155", port=25565)
+mc.postToChat("hola")
+# print(mc.getPlayerEntityId("Thejairex"))
